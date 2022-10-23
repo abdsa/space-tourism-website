c0290be26fbdb0a9a656d24f95f99e0b61cf870c
abdsa <abd.sab111@gmail.com>
2022-10-23 14:03:02 +0300

    fixed background

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import Header from "../components/header";
 import data from "../data.json";
 import types from "../data";
-
 export const config = {
   amp: true,
 };
@@ -12,14 +11,33 @@ export const config = {
 const Home: NextPage = () => {
   return (
     <>
-      <div className="grid grid-cols-[30%,70%]">
-        <nav>
-          <Header></Header>
-        </nav>
-        <main className="home-background-img"></main>
+      <nav>
+        <Header></Header>
+      </nav>
+      <main></main>
 
-        <footer></footer>
-      </div>
+      <footer></footer>
+
+      <style jsx global>
+        {`
+          body {
+            background: url("/background-home-mobile.jpg") no-repeat;
+            background-size: cover;
+          }
+
+          @media (min-width: 35rem) {
+            body {
+                background-image: url(/background-home-tablet.jpg);
+            }
+        }
+        
+        @media (min-width: 45rem) {
+          body {
+                background-image: url(/background-home-desktop.jpg);
+            }
+        }
+        `}
+      </style>
     </>
   );
 };
