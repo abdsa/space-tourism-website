2adcce0b5462aeb6d8678342325a8939095fb9de
abdsa <abd.sab111@gmail.com>
2022-10-25 05:37:09 +0300

    optimizing

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
     <>
       <Header></Header>
 
-      <main className="grid grid-flow-col grid-cols-1 grid-rows-2 desktop:grid-cols-2 desktop:grid-rows-1">
+      <main className="grid grid-flow-col grid-cols-1 grid-rows-2 desktop:grid-cols-2 desktop:grid-rows-1 pb-16">
         <div className="flex flex-col self-center justify-self-center">
           <h1 className="text-center font-barlow text-navText tracking-smHeading text-secondary desktop:text-start desktop:text-smHeading">
             SO, YOU WANT TO TRAVEL TO
