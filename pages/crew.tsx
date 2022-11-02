70c324e719b34ff11dfa829b1bf38d9cb1427993
abdsa <abd.sab111@gmail.com>
2022-11-02 08:25:58 +0300

    Opt

--- pages/crew.tsx
+++ pages/crew.tsx
@@ -39,7 +39,7 @@ const Home: NextPage = () => {
         id="main"
         className="relative grid grid-rows-[repeat(3,min-content)] gap-10 pb-12 font-bellefair text-white tablet:gap-10 desktop:place-content-center desktop:justify-items-start desktop:gap-0"
       >
-        <h1 className="justify-self-center font-barlowC text-navText font-thin uppercase tracking-smHeading tablet:ml-20 tablet:justify-self-start tablet:text-smHeading desktop:text-smHeading">
+        <h1 className="justify-self-center font-barlowC text-navText font-thin uppercase tracking-smHeading tablet:ml-20 tablet:justify-self-start desktop:ml-0 tablet:text-smHeading desktop:text-smHeading">
           <span className="mr-5 font-bold text-[rgb(255,255,255,0.25)]">
             02
           </span>
