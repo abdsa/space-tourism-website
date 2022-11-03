7ff0f7a93b457305a8fa1524c808d9d2dc16604b
abdsa <abd.sab111@gmail.com>
2022-11-03 10:49:50 +0300

    postcss.config edit

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,10 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
-  },
-};
+module.exports = {  
+  plugins: [  
+      require('tailwindcss'),  
+      require('postcss-nested'),  
+      require('autoprefixer'),
+      require('cssnano')({  
+          preset: 'default'  
+      })  
+  ]
+}
